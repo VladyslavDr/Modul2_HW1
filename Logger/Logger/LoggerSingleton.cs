d60c5f07934e9a7ed6fe915874dbfc19ee6e8bb2
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-10 13:48:35 +0300

    add LoggerSingleton

---
+++ Logger/Logger/LoggerSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class LoggerSingleton
+    {
+        private static LoggerSingleton _instance;
+
+        private LoggerSingleton()
+        {
+        }
+
+        public static LoggerSingleton GetInstance()
+        {
+            if (_instance is null)
+            {
+                _instance = new LoggerSingleton();
+            }
+
+            return _instance;
+        }
+    }
+}
